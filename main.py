a2cb9d22557f38e0c4d7d50ed4c39b92502df9ee
h-quest <71268696+h-quest@users.noreply.github.com>
2021-06-30 14:55:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -146,5 +146,5 @@ with figures:
       
         #st.table(info)
         
-        st.image('Fault_classficiation_table.png', width=0.7)
+        st.image('Fault_classficiation_table.png', width=1)
 
